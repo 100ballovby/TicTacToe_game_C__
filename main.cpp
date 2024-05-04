60ce88376d5b46cc53d8f54d42abdeb9f1396b41
GreatRaksin <greatraksin@icloud.com>
2024-05-04 17:29:52 +0300

    Adding main() function logic 🎁

--- main.cpp
+++ main.cpp
@@ -25,5 +25,25 @@ int computerMove(const vector<char> &board, char computer); // узнает хо
 void announceWinner(char winner, char computer, char human); // Поздравляет победителя или объявляет ничью
 
 int main() {
+    int move;
+    const int NUM_SQUARES = 9;
+    vector<char> board(NUM_SQUARES, EMPTY);
+    instructions();
+    char human = humanPiece();
+    char computer = opponent(human);
+    char turn = X;
+    displayBoard(board);
+    while (winner(board) == NO_ONE) {
+        if (turn == human) {
+            move = humanMove(board, human);
+            board[move] = human;
+        } else {
+            move = computerMove(board, computer);
+            board[move] = computer;
+        }
+        displayBoard(board);
+        turn = opponent(turn);
+    }
+    announceWinner(winner(board), computer, human);
     return 0;
 }
